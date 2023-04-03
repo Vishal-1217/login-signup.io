8f4ca3c95966594bfe020f1f4a36ee528ba0ab64
Vishal-1217 <vishalrajendra17@gmail.com>
2023-04-04 05:00:28 +0530

    login
    login-sign-app

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SigninComponent } from './signin/signin.component';
+
+const routes: Routes = [
+  {
+    path:'', redirectTo:'login', pathMatch:'full'
+  },
+  {
+    path:'login', component:LoginComponent
+  },
+  {
+    path:'signin', component:SigninComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SigninComponent } from './signin/signin.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    SigninComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
